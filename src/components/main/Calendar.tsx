09a58453fc64488039b7e26669630f65e84caf25
seondal <sseondal@gmail.com>
2022-11-27 05:42:33 +0900

    feat : 날짜 데이터 calendar 이중배열에 저장

--- src/components/main/Calendar.tsx
+++ src/components/main/Calendar.tsx
@@ -5,27 +5,34 @@ import React, { useState } from "react";
 interface DateData {
   full: string;
   date: string;
-  day: number;
 }
 
 export const MonthCalendar = ({ selected }: { selected: moment.Moment }) => {
-  let calendar: DateData[] = [];
-
   let date = selected.clone().startOf("month");
 
+  let dates: DateData[] = [];
+
+  // 1일 이전의 날짜들 처리
   for (let i = date.day(); i > 0; i--) {
-    calendar.unshift({ full: `${i}`, date: `none`, day: i });
+    dates.unshift({ full: `${i}`, date: `none` });
   }
 
-  for (let i = 0; i < selected.clone().endOf("month").date(); i++) {
+  // 날짜 데이터 넣기
+  for (; date < selected.clone().endOf("month"); date.add(1, "day")) {
     const data: DateData = {
-      full: date.format("YYYY-MM-DD"),
-      date: date.format("DD"),
-      day: date.day(),
+      full: date.format("YYYY-MM-DD"), // href 쿼리에 전달할 연도-월-날짜
+      date: date.format("DD"), // 화면에 표시되는 날짜
     };
-    calendar = calendar.concat(data);
-    date.add(1, "day");
+    dates = dates.concat(data);
   }
+
+  // [주][날짜] 형태의 이중배열로 저장
+  let calendar: string[][] = Array.from(Array(6), () => new Array(7));
+  dates.forEach((date, index) => {
+    calendar[Math.floor(index / 7)][index % 7] = date.date;
+  });
+
+  console.table(calendar);
 
   return (
     <div>
@@ -37,19 +44,17 @@ export const MonthCalendar = ({ selected }: { selected: moment.Moment }) => {
         <th>Thu</th>
         <th>Fri</th>
         <th>Sat</th>
-        <tr>
-          {calendar.map((day) =>
-            day.date !== "none" ? (
-              <td key={day.full}>
-                <Link href={`/main?date=${day.full}`}>
-                  <span>{day.date}</span>
-                </Link>
-              </td>
-            ) : (
-              <td></td>
-            )
-          )}
-        </tr>
+        {dates.map((day) => {
+          return day.date !== "none" ? (
+            <td key={day.full}>
+              <Link href={`/main?date=${day.full}`}>
+                <span>{day.date}</span>
+              </Link>
+            </td>
+          ) : (
+            <td></td>
+          );
+        })}
       </table>
     </div>
   );
